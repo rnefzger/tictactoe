8b0adf21ab43ab24654fea344704054f2803a5fb
Richard Nefzger <richard.nefzger@hm.edu>
2021-05-18 09:46:14 +0200

    Implement Player

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,4 +1,14 @@
 package game;
 
 public class Player {
+
+    private final int id;
+
+    Player(final int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
