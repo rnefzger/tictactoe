cf9bf72a98d407d5cacdd41272354a9a4e34ba2d
Richard Nefzger <richard.nefzger@hm.edu>
2021-05-18 15:52:38 +0200

    Add belongTo to class Field and update it in connections.puml

--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -1,19 +1,28 @@
 package game;
 
 import javax.annotation.processing.Generated;
+import java.util.Objects;
 
 public class Field {
 
-    private boolean isFilled;
     private final int index;
+    private boolean isFilled;
+    private Player belongsTo;
 
     Field(final int index) {
         this.index = index;
         isFilled = false;
+        belongsTo = new Player(2);
     }
 
+    @Generated({})
     public void setFilled(final boolean filled) {
         isFilled = filled;
+    }
+
+    @Generated({})
+    public void setBelongsTo(Player belongsTo) {
+        this.belongsTo = belongsTo;
     }
 
     @Generated({})
@@ -22,7 +31,27 @@ public class Field {
     }
 
     @Generated({})
+    public Player getBelongsTo() {
+        return belongsTo;
+    }
+
+    @Generated({})
     public boolean isFilled() {
         return isFilled;
+    }
+
+    @Override
+    @Generated({})
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Field field = (Field) o;
+        return index == field.index;
+    }
+
+    @Override
+    @Generated({})
+    public int hashCode() {
+        return Objects.hash(index);
     }
 }
