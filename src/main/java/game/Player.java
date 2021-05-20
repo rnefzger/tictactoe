b776d0b8bde3b456ce79d67578a8a9c0a7956eab
Richard Nefzger <richard.nefzger@hm.edu>
2021-05-20 21:55:53 +0200

    Add tests for class Player, implement it and add comments in Player

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,7 +1,15 @@
 package game;
 
-public class Player {
+import java.util.Objects;
 
+/**
+ * Represents a player in the game
+ */
+public abstract class Player {
+
+    /**
+     * The unique identifier of this player
+     */
     private final int id;
 
     Player(final int id) {
@@ -10,5 +18,42 @@ public class Player {
 
     public int getId() {
         return id;
+    }
+
+    /**
+     * Carry out a turn of the current player
+     *
+     * @param index to be conquered
+     * @return the index to conquer
+     */
+    public abstract int turn(final int index);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return id == player.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    /**
+     * The sign of the this player
+     *
+     * @return sign of this player
+     */
+    @Override
+    public String toString() {
+        if (id == 0) {
+            return "X";
+        } else if (id == 1){
+            return "O";
+        }
+
+        return " ";
     }
 }
