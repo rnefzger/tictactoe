cf82e51340cbd656d8641d23298f93cbbc8997ee
Richard Nefzger <richard.nefzger@hm.edu>
2021-05-21 00:17:44 +0200

    Refactor code

--- src/main/java/game/Computer.java
+++ src/main/java/game/Computer.java
@@ -1,26 +1,23 @@
 package game;
 
-import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Represents a computer
  */
 public class Computer extends Player {
 
-    Computer(int id) {
+    Computer(final int id) {
         super(id);
     }
 
     /**
      * Carry out a turn of the computer
      *
-     * @param index to be conquered
      * @return the index to conquer
      */
-    //TODO
     @Override
-    public int turn(final int index) {
-        //TODO: Kann kleiner als 0 werden
-        return (int) (Math.random() * 10 - 1);
+    public int turn() {
+        return new Random().nextInt(8);
     }
 }
